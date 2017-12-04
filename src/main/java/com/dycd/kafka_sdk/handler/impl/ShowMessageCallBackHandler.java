41d7a73bc6c43022efbbe6d32f55532dd4f457ba
he.chen <chenhe@dycd.com>
2017-12-04 16:15:35 +0800

    kafka producer

--- src/main/java/com/dycd/kafka_sdk/handler/impl/ShowMessageCallBackHandler.java
+++ src/main/java/com/dycd/kafka_sdk/handler/impl/ShowMessageCallBackHandler.java
@@ -1,7 +1,12 @@
 package com.dycd.kafka_sdk.handler.impl;
 
 import com.dycd.kafka_sdk.handler.MessageCallBackHandler;
+import kafka.utils.Json;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import scala.util.parsing.json.JSON;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShowMessageCallBackHandler implements MessageCallBackHandler {
 
@@ -9,8 +14,12 @@ public class ShowMessageCallBackHandler implements MessageCallBackHandler {
 	public void callBack(Object o) {
 		// TODO Auto-generated method stub
 		RecordMetadata meta = (RecordMetadata)o;
-		System.out.println(meta.offset() + " " + meta.partition() + " " + meta.topic());
-		System.out.println(meta.toString());
+//		System.out.println(meta.offset() + " " + meta.partition() + " " + meta.topic());
+		Map<String,Object> params = new HashMap<>(16);
+		params.put("offset",meta.offset());
+		params.put("partition",meta.partition());
+		params.put("topic",meta.topic());
+		System.out.println(com.alibaba.fastjson.JSON.toJSON(params));
 	}
 
 }
